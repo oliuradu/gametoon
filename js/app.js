9cd5b93b9260ecbd6308c3c61bf88434dfb467a4
Oliu Radu <oliuradu@gmail.com>
2019-02-14 21:34:57 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+var game0button = document.querySelector("#game0").addEventListener("click", openGame0);
+var game1button = document.querySelector("#game1").addEventListener("click", openGame1);
+
+
+// Choice the number game
+import { openGame0 } from './games/guessthenumber.js';
+
+// Wingman
+
+import { openGame1 } from './games/wingman.js';
+
+
+/// Confeti
